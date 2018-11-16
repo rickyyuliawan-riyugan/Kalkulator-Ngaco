da9c3d1304f2eda183917bbedd1cdf33d92c1ff2
Mohammad Ridwan Hady Arifin <ridwanhady31@gmail.com>
2018-11-16 14:19:11 +0700

    Update spek Token.c
    sisanya menyusul

--- token.c
+++ token.c
@@ -1,46 +1,66 @@
+/* Identifier : semua character a..z atau A..Z atau '_' */
+/* Digit : semua angka mulai dari 0..9 dan '.' */
+/* Number : semua kombinasi dari digit digit yang tersedia atau ditambah dengan symbol + atau - */
+/* Symbol : ^ * / + - */
+
+/* Apabila token berisi sebuah number maka tok.sbl = 'b' */
+/* Apabila token berisi sebuah identifier maka tok.sbl = 'i' */
+/* Apabila token berisi sebuah stop maka tok.sbl = 's' */
+
+/*Spek lainnya akan ditulis lebih lanjut di waktu yang lain, semoga yang ini cukup jelas */
+
 typedef struct{
-    int Mem[100];
-    int Neff;
-} Array;
-typedef char* string;
-typedef struct{
-   string token;
-   int posnow;
+   int pos;
+   char* text;
+   char* tkn;
+   char sbl;
+   double value;
 } Token;
+typedef struct{
+    Token token;
+} ArrayOfToken;
 
-boolean isNumber();
+boolean isNumber(Token *tok);
+/* Mengembalikan 'true' apabila tok.tkn == "number" */
 
-boolean isSymbol();
+boolean isSymbol(Token *tok, char s);
+/* Mengembalikan 'true' apabila tok.tkn == "symbol" DAN tok.sbl == s */
 
-boolean isIdentifier();
+boolean isIdentifier(Token *tok);
+/* Mengembalikan 'true' apabila tok.tkn == "identifier" */
 
-void number();
+boolean isStop(Token *tok);
+/* Mengembalikan 'true' apabila tok.tkn == "stop" */
 
-void symbol();
+Token number(char* text, int pos);
+/* Mengembalikan Token dengan nilai tok.text = text, tok.pos = pos, dan tok.tkn = "number" */
 
-void identifier();
+Token symbol(char* text, int pos);
+/* Mengembalikan Token dengan nilai tok.text = text, tok.pos = pos dan tok.tkn = "symbol" */
 
-void stop();
+Token identifier(char* text, int pos);
+/* Mengembalikan Token dengan nilai tok.text = text, tok.pos = pos dan tok.tkn = "identifier" */
+
+Token stop(int pos);
+/* Mengembalikan Token dengan nilai tok.text = "" tok.pos = pos dan tok.tkn = "stop" */
 
 boolean isIdentifier(char ch)
+/* Mengembalikan 'true' jika ch adalah sebuah Identifier, lihat definisi Identifier diatas */
 {
     return (('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_');
 }
 
 boolean isDigit(char ch)
+/* Mengembalikan 'true' jika ch adalah sebuah Digit, lihat definisi Digit diatas */
 {
     return ((ch == '0') || (ch == '1') || (ch == '2') || (ch == '3') || (ch == '4') || (ch == '5') ||(ch == '6') || (ch == '7') ||(ch == '8') || (ch == '9') ||(ch == '.'));
 }
 
-Token nextToken(string s, int position)
-{
-    while position < strlen(s){
-        position += 1;
-    }
-    int start = position;
-}
+Token nextToken(string s, int position);
+/* Mengembalikan dan membaca token selanjutnya */
 
-Array tokenize(string s)
+ArrayOfToken tokenize(string s)
+/* Mengembalikan sebuah ArrayOfToken dari hasil pembacaan token */
 {
     Array tokens;
     int position = 0;
